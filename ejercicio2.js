958dda58ab177fabad7734ee0d39bffafb796bad
Michael Stiven Echeverry Vargas <985hecheverrymichael@gmail.com>
2026-02-27 16:53:26 -0500

    app.js eliminado

---
+++ ejercicio2.js
@@ -0,0 +1,17 @@
+// 2. Área de un rectángulo
+// inicio
+
+// declaramos las variables
+let base, altura, area;
+
+// datos de ingreso
+base = parseFloat(prompt("Ingrese la base del rectángulo"));
+altura = parseFloat(prompt("Ingrese la altura del rectángulo"));
+
+// operaciones
+area = base * altura;
+
+// datos de salida
+alert("El área del rectángulo es " + area);
+
+// fin
