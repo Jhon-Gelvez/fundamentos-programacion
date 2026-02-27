8fedc51e7ab21b6eb9e541da9f351eb59e25c4ef
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-02-27 16:16:18 -0500

    ejercicios 6 y 7

---
+++ app.js
@@ -0,0 +1,30 @@
+// 6 calcular perimetro de un cuadrado
+// inicio
+
+// declaramos las variables
+let lado, perimetro;
+// datos de ingreso
+lado = parseInt(prompt("ingrese la longitud de un lado del cuadrado"));
+// operaciones
+perimetro = lado * 4;
+// datos de salida
+alert("perimetro " + perimetro);
+// fin
+
+// 7. Calcular el doble y el triple
+
+// inicio
+
+// declaramos las variables
+let numero, doble, triple;
+// datos de ingreso
+numero = parseInt(prompt("ingrese un numero"));
+
+// operaciones
+doble = numero * 2;
+triple = numero * 3;
+
+// datos de salida
+alert("el doble del numero es " + doble);
+alert("el triple del numero es " + triple);
+// fin
