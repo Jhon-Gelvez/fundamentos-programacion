bb60ad0ba27d03e49c9109b82e076dc89693fa88
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-02-27 16:51:07 -0500

    archivos 6 y 7 separados y enlazados por medio de links cambio de nombre a app.js por 7.js

---
+++ 6.js
@@ -0,0 +1,12 @@
+// 6 calcular perimetro de un cuadrado
+// inicio
+
+// declaramos las variables
+let lado, perimetro;
+// datos de ingreso
+lado = parseInt(prompt("ingrese la longitud de un lado del cuadrado"));
+// operaciones
+perimetro = lado * 4;
+// datos de salida
+alert("perimetro " + perimetro);
+// fin
--- 7.js
+++ 7.js
@@ -1,15 +1,4 @@
-// 6 calcular perimetro de un cuadrado
-// inicio
 
-// declaramos las variables
-let lado, perimetro;
-// datos de ingreso
-lado = parseInt(prompt("ingrese la longitud de un lado del cuadrado"));
-// operaciones
-perimetro = lado * 4;
-// datos de salida
-alert("perimetro " + perimetro);
-// fin
 
 // 7. Calcular el doble y el triple
 
